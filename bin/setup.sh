04ec80e05fe1dfd51fb47024f8edf0c2c3c32484
Hunter Werner <htwerner@gmail.com>
2020-09-14 23:02:46 -0400

    Updated interface and additional apps

--- bin/setup.sh
+++ bin/setup.sh
@@ -19,3 +19,8 @@ sudo systemctl enable docker
 sudo apt install curl
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Install node.js
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.18.2 | sudo -E bash -
+sudo apt -y install nodejs
