50c572999f94bf7abcb8175d3450d911d2274a0a
Hunter Werner <htwerner@gmail.com>
2020-09-14 22:54:32 -0400

    Updated interface and additional apps

--- bin/setup.sh
+++ bin/setup.sh
@@ -19,6 +19,3 @@ sudo systemctl enable docker
 sudo apt install curl
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-# Install gradle
-sudo apt install gradle
